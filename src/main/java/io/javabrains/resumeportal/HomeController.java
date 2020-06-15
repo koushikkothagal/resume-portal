cd045bf34756dd515385a91a5fd2e5ab914be576
Koushik Kothagal <koush.s@gmail.com>
2020-06-15 14:50:25 -0700

    Added a profile view path + Thymeleaf

--- src/main/java/io/javabrains/resumeportal/HomeController.java
+++ src/main/java/io/javabrains/resumeportal/HomeController.java
@@ -1,9 +1,11 @@
 package io.javabrains.resumeportal;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
@@ -14,5 +16,11 @@ public class HomeController {
     @GetMapping("/edit")
     public String edit() {
         return "edit page";
+    }
+
+    @GetMapping("/view/{userId}")
+    public String view(@PathVariable String userId, Model model) {
+        model.addAttribute("userId", userId);
+        return "profile";
     }
 }
