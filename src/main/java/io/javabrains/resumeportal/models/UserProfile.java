f7001cfbe2c90331abe12e82ed6f959a0b6a1ced
Koushik Kothagal <koush.s@gmail.com>
2020-06-15 16:08:34 -0700

    Added basic user profile elements and displayed in templates

--- src/main/java/io/javabrains/resumeportal/models/UserProfile.java
+++ src/main/java/io/javabrains/resumeportal/models/UserProfile.java
@@ -11,6 +11,51 @@ public class UserProfile {
     private String userName;
     private int theme;
     private String summary;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String designation;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
 
     public String getUserName() {
         return userName;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into user (id, user_name, password, active, roles) values
-(1, 'foo', 'foo', 'true', 'USER'),
-(2, 'bar', 'bar', 'true', 'USER');
+(1, 'einstein', 'einstein', 'true', 'USER'),
+(2, 'newton', 'newton', 'true', 'USER');
 
-insert into user_profile (id, user_name, theme, summary) values
-(1, 'foo', '1', 'User name foo'),
-(2, 'bar', '2', 'User name bar');
+insert into user_profile (id, user_name, theme, summary, first_name, last_name, email, phone, designation) values
+(1, 'einstein', '1', 'Developed the theory of relativity, one of the two pillars of modern physics. My work is also known for its influence on the philosophy of science.', 'Albert', 'Einstein', 'einstein@gmail.com', '111-111-1111', 'Theoretical physicist'),
+(2, 'newton', '2', 'Widely recognised as one of the most influential scientists of all time and as a key figure in the scientific revolution', 'Isaac', 'Newton', 'newton@gmail.com', '222-222-2222', 'Mathematician, physicist, astronomer, theologian, and author');
