9d8ae25a0b83eefec89d7279310513d3b25eb231
Koushik Kothagal <koush.s@gmail.com>
2020-06-19 16:50:32 -0700

    Added a welcome page and removed db init code

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 insert into user (id, user_name, password, active, roles) values
 (1, 'einstein', 'einstein', 'true', 'USER'),
-(2, 'newton', 'newton', 'true', 'USER');
+(2, 'newton', 'newton', 'true', 'USER'),
+(3, 'koushik', 'koushik', 'true', 'USER');
 
 insert into user_profile (id, user_name, theme, summary, first_name, last_name, email, phone, designation) values
 (1, 'einstein', '1', 'Developed the theory of relativity, one of the two pillars of modern physics. My work is also known for its influence on the philosophy of science.', 'Albert', 'Einstein', 'einstein@gmail.com', '111-111-1111', 'Theoretical physicist'),
